04e786a51a48e827fbab39e45e2070c195d26dd9
Musa Azeem <69978213+Musa-Azeem@users.noreply.github.com>
2022-11-15 17:01:32 -0500

    clean code - switch output to ptc1

--- lightsensor.cpp
+++ lightsensor.cpp
@@ -10,7 +10,8 @@ InterruptIn STOP_BUTTON(PTA12);
 DigitalOut RED_LED(LED1);
 DigitalOut GREEN_LED(LED2);
 DigitalOut BLUE_LED(LED3);
-DigitalOut OUTPUT(PTC2);
+DigitalOut OUTPUT_LIGHT(PTC1);
+
 
 #define Vsupply 3.3f //microcontroller voltage supply 3.3V
 
@@ -20,24 +21,10 @@ float LightSensorVoltValue; //voltage on the controller input pin
 float LdrResistance; //photoresistance value
 #define LdrBiasResistor 3300.0f //Bias resistor (upper leg of voltage divider) for LDR
 
-//variables for temperature sensor
-float TemperatureSensorDigiValue; //the A/D converter value read by the controller input pin
-float TemperatureSensorVoltValue; //the voltage on the controller input pin (across the 10k resistor) from the temperature sensor voltage divider
-float ThermistorResistance; //computed from the voltage drop value across the thermistor
-float ThermistorTemperature; //approximate ambient temperature measured by thermistor
-#define ThermistorBiasResistor 10000.0f //Bias resistor (lower leg of voltage divider) for thermistor
-
-//variables for torque sensor
-float MotorCurrentDigiValue; //the A/D converter value ready by the controller input pin
-float MotorCurrentVoltValue; //the voltage on the controller input pin (across the 10 ohm resistor) from the motor torque sensor
-float MotorCurrent; //computed from the voltage value
-#define MotorSeriesResistance 10.0f //resistance of torque (current) sensing resistor in series with the Motor
 
 // Variables to hold control reference values.
 // STUDENT: EDIT THESE VALUES
 float LightResistanceLimit = 25000.0; //enter a resistance reference for LDR load activation
-float TemperatureLimit = 20.0; //enter a temperature in Celsius here for temperature deactivation; NOTE: room temperature is 25C
-float MotorCurrentLimit = 0.1; //enter a reference current in amperes for motor torque deactivation
 
 // This function will be attached to the start button interrupt.
 void StartPressed(void)
@@ -45,7 +32,7 @@ void StartPressed(void)
     cout << "Start!" << endl;
     RED_LED = 1;
     GREEN_LED = 0;
-    OUTPUT = 1;
+    OUTPUT_LIGHT = 1;
 }
 
 // This function will be attached to the stop button interrupt.
@@ -55,7 +42,7 @@ void StopPressed(void)
     cout << "Stop!" << endl;
     GREEN_LED = 1;
     RED_LED = 0;
-    OUTPUT = 0;
+    OUTPUT_LIGHT = 0;
     
 }
 
@@ -77,90 +64,24 @@ void CheckLightSensor(void)
 {
     if(getPhotoResistance() >= LightResistanceLimit) {
         cout << "LDR Start!" << endl;
-        OUTPUT = 1;     // START
+        OUTPUT_LIGHT = 1;     // START
         RED_LED = 0;
         GREEN_LED = 1;
 
     }
     else {
-        OUTPUT = 0; // STOP
+        OUTPUT_LIGHT = 0; // STOP
         RED_LED = 1;
         GREEN_LED = 0;
     }
 }
-
-// This function converts the voltage value from the thermistor input to an approximate temperature
-// in Celsius based on a linear approximation of the thermistor.
-float getThermistorTemperature(void)
-{
-    // STUDENT: EDIT HERE
-    // 1. Read the TemperatureSensor A/D value and store it in TemperatureSensorDigiValue
-    // 2. Calculate TemperatureSensorVoltValue from TemperatureSensorDigiValue and Vsupply
-    // 3. Calculate ThermistorResistance using the voltage divider equation
-    
-    TemperatureSensorDigiValue = TemperatureSensor.read();
-    TemperatureSensorVoltValue = Vsupply * TemperatureSensorDigiValue;
-    ThermistorResistance = ThermistorBiasResistor*((Vsupply - TemperatureSensorVoltValue) / TemperatureSensorVoltValue);
-    ThermistorTemperature = ((ThermistorResistance - 10000.0)/(-320.0)) + 25.0; //temperature of the thermistor computed by a linear approximation of the device response
-
-    return ThermistorTemperature;
-}
-
-//This function will check for a temperature triggered deactivation of the motor
-void CheckTemperatureSensor(void)
-{
-    // STUDENT: EDIT HERE
-    // Use the getThermistorTemperature() function defined above to obtain a temperature value to use for comparison and decision making with your TemperatureLimit
-    if(getThermistorTemperature() >= TemperatureLimit) {
-        cout << "Thermistor Stop!" << endl;
-        OUTPUT = 0;
-        GREEN_LED = 1; // OFF
-        RED_LED = 0;
-    }
-    else {
-//        GREEN_LED = 0;
-//        RED_LED = 1;
-    }
-}
-
-//This function will determine the motor current in amperes
-float getMotorCurrent(void)
-{
-    // STUDENT: EDIT HERE
-    // 1. Read the TorqueSensor value and store it in MotorCurrentDigiValue
-    // 2. Calculate MotorCurrentVoltValue from MotorCurrentDigiValue and Vsupply
-    // 3. Calculate MotorCurrent using Ohm's law from MotorCurrentVoltValue and MotorSeriesResistance
-
-    MotorCurrentDigiValue = TorqueSensor.read();
-    MotorCurrentVoltValue = Vsupply * MotorCurrentDigiValue;
-    MotorCurrent = MotorCurrentVoltValue / MotorSeriesResistance;
-    return MotorCurrent;
-}
-
-// This function will check the Over Torque analog input.
-void CheckTorqueSensor(void)
-{
-    // STUDENT: EDIT HERE
-    // Use the getMotorCurrentValue() function defined above to obtain a current torque value to use for comparison and decision making with your MotorCurrentLimit
-    if(getMotorCurrent() >= MotorCurrentLimit) {
-        cout << "Current Stop!" << endl;
-        OUTPUT = 0;
-        GREEN_LED = 1;
-        RED_LED = 0;
-    }
-    else {
-//        GREEN_LED = 0;
-//        RED_LED = 1;
-    }
-}
-
 // Standard entry point in C++.
 int main(void)
 {
     // Attach the functions to the hardware interrupt pins.
     START_BUTTON.rise(&StartPressed);
     STOP_BUTTON.rise(&StopPressed);
-    // Initialize LED outputs to OFF (LED logic is inverted)
+    // Initialize LED OUTPUT_LIGHTs to OFF (LED logic is inverted)
     RED_LED = 1;
     GREEN_LED = 1;
     BLUE_LED = 1;
@@ -173,14 +94,10 @@ int main(void)
     while(true) {
         // Check the analog inputs.
         CheckLightSensor();
-//        CheckTemperatureSensor();
-//        CheckTorqueSensor();
         
         // Print Analog Values to screen
         cout << "\n\rLDR Resistance: " << getPhotoResistance() << endl;
-        cout << "\rCurrent Temperature Value: " << getThermistorTemperature() << endl;
-        cout << "\rMotor Current: " << getMotorCurrent() << endl;
-        cout << "\rOUTPUT: " << OUTPUT << endl;
+        cout << "\rOUTPUT_LIGHT PTC1: " << OUTPUT_LIGHT << endl;
         
         wait(1.0); // Wait 1 second before repeating the loop.
     }
